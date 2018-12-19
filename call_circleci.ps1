7da0a918b4f71424f7f8111a00ded36d623ea071
phucknguyen <phucknguyen@gmail.com>
2018-12-19 14:34:25 +0700

    Initial working version

---
+++ call_circleci.ps1
@@ -0,0 +1,34 @@
+# Kick off a build on circleci
+
+# Script parameters
+param (
+  [string]$token = $env:CIRCLECI_TOKEN
+)
+
+If ([string]::IsNullOrEmpty($token)) {
+  throw "Token is required as parameter or env variable CIRCLECI_TOKEN"
+}
+
+# fail on first error (e.g. directory not exist)
+$ErrorActionPreference = "Stop"
+
+$uriBase = "https://circleci.com/api/v1.1/project/github"
+$username = "phucnguyen81"
+$project = "ci-samples"
+$uri = "${uriBase}/${username}/${project}?circle-token=${token}"
+
+$headers = @{
+  'Content-Type' = 'application/json'
+}
+
+$body = @{
+  build_parameters = @{
+    DOWNLOAD_URL = 'https://phucknguyen-bucket.s3.amazonaws.com/ci-samples-2.zip'
+    COMMAND = 'katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/TS_RegressionTest"'
+  }
+}
+
+$bodyJson = $body | ConvertTo-Json
+
+Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $bodyJson
+
--- download-ci-samples.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-# Download s3 file and unzip it
- wget -O ci-samples.zip https://phucknguyen.s3.us-west-2.amazonaws.com/ci-samples.zip
- unzip ci-samples.zip
